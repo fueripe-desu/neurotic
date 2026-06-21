1a74cbaaa70dee5b3b68437a42bef1f8d4b9991d
fueripe-desu <fueripedesu2020@outlook.com>
2026-06-21 12:17:16 -0300

    feat: add Splash screen

--- src/main.py
+++ src/main.py
@@ -1,21 +1,17 @@
-from textual.app import App, ComposeResult
+from textual.app import App
 from textual.binding import Binding
-from textual.widgets import Footer, Header
+from splash.splash_screen import SplashScreen
 
 class NeuroticApp(App):
     """The main class for Neurotic memory management."""
 
+    SCREENS = { "splash": SplashScreen }
     BINDINGS = [
         Binding(key="q", action="quit", description="Quit")
     ]
 
-    def compose(self) -> ComposeResult:
-        yield Header()
-        yield Footer()
-
     def on_mount(self) -> None:
-        self.title = "Neurotic"
-        self.theme = "tokyo-night"
+        self.push_screen("splash");
 
 if __name__ == "__main__":
     app = NeuroticApp()
---
+++ src/splash/splash_screen.py
@@ -0,0 +1,24 @@
+from textual.app import ComposeResult
+from textual.screen import Screen
+from textual.binding import Binding
+from textual.widgets import Static, Footer
+
+class SplashScreen(Screen):
+    """The splash screen"""
+
+    CSS_PATH="splash_screen.tcss"
+
+    def compose(self) -> ComposeResult:
+        yield Static(r" /$$   /$$                                           /$$     /$$          ", classes="display-text")
+        yield Static(r"| $$$ | $$                                          | $$    |__/          ", classes="display-text")
+        yield Static(r"| $$$$| $$  /$$$$$$  /$$   /$$  /$$$$$$   /$$$$$$  /$$$$$$   /$$  /$$$$$$$", classes="display-text")
+        yield Static(r"| $$ $$ $$ /$$__  $$| $$  | $$ /$$__  $$ /$$__  $$|_  $$_/  | $$ /$$_____/", classes="display-text")
+        yield Static(r"| $$  $$$$| $$$$$$$$| $$  | $$| $$  \__/| $$  \ $$  | $$    | $$| $$      ", classes="display-text")
+        yield Static(r"| $$\  $$$| $$_____/| $$  | $$| $$      | $$  | $$  | $$ /$$| $$| $$      ", classes="display-text")
+        yield Static(r"| $$ \  $$|  $$$$$$$|  $$$$$$/| $$      |  $$$$$$/  |  $$$$/| $$|  $$$$$$$", classes="display-text")
+        yield Static(r"|__/  \__/ \_______/ \______/ |__/       \______/    \___/  |__/ \_______/", classes="display-text")
+        yield Footer(show_command_palette=False)
+
+    def on_mount(self) -> None:
+        self.theme = "tokyo-night"
+
